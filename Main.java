e063cfdc2c7ff0704096fdbd68837904f3c03a77
anb2473 <austinnabilblass@gmail.com>
2025-03-30 21:52:30 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ import Kernel.Azura.AzuraCompiler;
 public class Main {
     public static void main(String[] args) {
         AzuraCompiler compiler = new AzuraCompiler();
-        compiler.execute("src/TestScripts/test.aura", null);
+        compiler.execute("src/TestScripts/test.azu", null);
         System.out.println(compiler.getData());
     }
 }
